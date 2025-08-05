612dc7db14f0189fef0fc1ce204df3c752bfd625
Shiwei Zhang <shizh@microsoft.com>
2025-08-05 17:45:12 +0800

    feat: scaffold project
    Signed-off-by: Shiwei Zhang <shizh@microsoft.com>

---
+++ OrasProject.OrasDesktop/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace OrasProject.OrasDesktop.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ OrasProject.OrasDesktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace OrasProject.OrasDesktop.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
